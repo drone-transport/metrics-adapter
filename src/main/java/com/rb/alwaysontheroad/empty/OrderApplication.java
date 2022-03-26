eb9f845a8181020a48a7c87c88b423bb21e95b7c
Rustam Mamedov <rbmamedo@mts.ru>
2022-03-26 20:38:23 +0300

    renamed project

--- src/main/java/com/rb/alwaysontheroad/empty/OrderApplication.java
+++ src/main/java/com/rb/alwaysontheroad/empty/OrderApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class EmptyApplication {
+public class OrderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EmptyApplication.class, args);
+        SpringApplication.run(OrderApplication.class, args);
     }
 }
---
+++ src/main/java/com/rb/alwaysontheroad/empty/api/routes/OrderApiRoutes.java
@@ -0,0 +1,6 @@
+package com.rb.alwaysontheroad.empty.api.routes;
+
+public interface OrderApiRoutes {
+    String ROOT_URL = "/api/order";
+    String ORDER_CREATE_URL = ROOT_URL + "/create";
+}
